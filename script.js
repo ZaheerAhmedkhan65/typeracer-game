6ff844271f3bdaf151824d4623e4efbdcf968a9c
Zaheer Ahmed <ranazaheerahmed65@gmail.com>
2024-12-04 09:18:12 +0500

    updated with car selection

--- script.js
+++ script.js
@@ -100,7 +100,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const minutes = Math.floor(seconds / 60);
         const secs = seconds % 60;
         return `${minutes.toString().padStart(2, '0')}:${secs.toString().padStart(2, '0')}`;
-      }
+    }
 
     const updateTimer = () => {
         if (timeLeft > 0) {
